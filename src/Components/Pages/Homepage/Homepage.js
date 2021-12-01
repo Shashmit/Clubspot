14c9d022d8a6fd5af3b85618221902a5ff140a47
Shashmit kumar <70282239+Shashmit@users.noreply.github.com>
2021-12-01 23:20:44 +0530

    Add files via upload

--- src/Components/Pages/Homepage/Homepage.js
+++ src/Components/Pages/Homepage/Homepage.js
@@ -7,9 +7,9 @@ function Homepage() {
                 <div class="front-col-1">
                     <div className="style-1">
                         <h1 class="value-1">Club<span>Spot</span></h1>
+                        <p>An event management platform for VIT-AP</p>
                         <div class="style-2"></div>
-                    </div>
-                    
+                    </div>   
                 </div>
                 <section class="event-slider">
                 <div class="slider-heading">
@@ -21,10 +21,24 @@ function Homepage() {
                   <strong>Heading</strong>  
                   <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"></img>
                   <p>About the event</p>
+                  </a>
+                </div>
+                </section>
+                <section class="event-slider">
+                <div class="slider-heading">
+                    <h3 class="top">
+                    REGISTRATION OPEN</h3>
+                </div>
+                <div className="event-box">
+                <a href ='#'>
+                  <strong>Heading</strong>  
+                  <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"></img>
+                  <p>About the event</p>
 
                   </a>
                 </div>
                 </section>
+                
             </div>         
     )
 }
