c459f4312f6e09ce07e76dee50a6c41afdd3de03
Shashmit kumar <70282239+Shashmit@users.noreply.github.com>
2022-01-12 22:01:26 +0530

    Add files via upload

---
+++ src/Components/Pages/Club/Club.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Clubs.css"
+
+function Club() {
+    return (
+        <div>
+            <div className="vit">
+            <h2 className='add'>VIT-AP University</h2>
+            </div>
+            <div class="parent-parallel">
+                <div class="parallel-1">
+                <p>shahs</p>
+                </div>
+                <div class="parallel-2">
+                <p>shahs</p>
+                </div>
+            </div>
+        </div>
+    )   
+}
+export default Club;
