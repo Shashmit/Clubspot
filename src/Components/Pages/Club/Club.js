8d6366a6d9dd39e90dd9d1e719568cd87732b9b1
Shashmit kumar <70282239+Shashmit@users.noreply.github.com>
2022-01-18 00:08:31 +0530

    Add files via upload

--- src/Components/Pages/Club/Club.js
+++ src/Components/Pages/Club/Club.js
@@ -3,19 +3,113 @@ import "./Clubs.css"
 
 function Club() {
     return (
-        <div>
-            <div className="vit">
-            <h2 className='add'>VIT-AP University</h2>
-            </div>
+      <div className='parent'>
             <div class="parent-parallel">
                 <div class="parallel-1">
-                <p>shahs</p>
+                  <h2 className='des'>Technical clubs</h2>   
+                    <div class="columns">
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    </div>
                 </div>
                 <div class="parallel-2">
-                <p>shahs</p>
+                  <h2 className='des'>Non-technical clubs</h2>
+                    <div class="columns">
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                    <a href ='#'>
+                      <div className="club-box">
+                          <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="club-logo"></img>
+                        <h3 id="">Club name</h3>
+                      </div>
+                      </a>
+                </div>
                 </div>
             </div>
-        </div>
+            </div>
+       
+        
+        
     )   
 }
 export default Club;
